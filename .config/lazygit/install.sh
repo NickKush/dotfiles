db2c3e1554f85c54d1e5c5e0eeaf3e38c45ea4b4
NickKush <nickush42@gmail.com>
2022-04-21 12:36:01 +0300

    (lazygit) Add bash script for lazygit install

---
+++ .config/lazygit/install.sh
@@ -0,0 +1,27 @@
+# Download the latest version of lazygit
+# https://github.com/jesseduffield/lazygit
+# 
+# We can't download it directly from PPA, because it's been deprecated
+
+
+# Make sure we start this script via sudo/root 
+if [[ $EUID -ne 0 ]]; then
+   echo "install.sh must be run as root"
+   exit 1
+fi
+
+
+TEMP_FOLDER="/tmp/lazygit_download"
+
+LAZYGIT_URL=$(curl -s https://api.github.com/repos/jesseduffield/lazygit/releases/latest \
+	| grep "browser_download_url" \
+	| grep Linux_x86_64.tar.gz \
+	| cut -d '"' -f 4
+)
+
+mkdir -p ${TEMP_FOLDER} && \
+	curl -s -L --create-dirs -o ${TEMP_FOLDER}/lazygit.tar.gz "$LAZYGIT_URL" && \
+	tar -xf ${TEMP_FOLDER}/lazygit.tar.gz -C ${TEMP_FOLDER} lazygit && \
+	cp ${TEMP_FOLDER}/lazygit /usr/local/bin/lazygit
+
+rm -rf ${TEMP_FOLDER}
