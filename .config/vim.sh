301756236f161b1462b0c162dc19b6b40e258a11
NickKush <nickush42@gmail.com>
2022-04-21 22:22:28 +0300

    (nvim) Add script to install and link neovim

---
+++ .config/vim.sh
@@ -0,0 +1,14 @@
+
+# Make sure we start this script with sudo/root 
+if [[ $EUID -ne 0 ]]; then
+   echo "install.sh must be run as root"
+   exit 1
+fi
+
+# Install neovim
+apt update && \
+  DEBIAN_FRONTEND=noninteractive apt --no-install-recommends install -yq \
+    neovim
+
+# Link neovim in vim shortcut
+ln -sf $(which nvim) /usr/local/bin/vim
