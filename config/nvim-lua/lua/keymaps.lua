ab3408b7c3e97330e9a4f04af1d52e9e75e9a315
NickKush <nickush42@gmail.com>
2022-06-22 16:17:30 +0300

    (nvim) more commands

--- config/nvim-lua/lua/keymaps.lua
+++ config/nvim-lua/lua/keymaps.lua
@@ -9,7 +9,6 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 
-
 -- Disable arrow keys
 keymap('', '<up>', '<nop>', opts)
 keymap('', '<down>', '<nop>', opts)
@@ -21,6 +20,21 @@ keymap('n', '<c-j>', '<c-w>j', opts)
 keymap('n', '<c-h>', '<c-w>h', opts)
 keymap('n', '<c-k>', '<c-w>k', opts)
 keymap('n', '<c-l>', '<c-w>l', opts)
+
+-- Resize with arrows keys
+keymap('n', '<c-up>',    ':resize -4<cr>', opts)
+keymap('n', '<c-down>',  ':resize +4<cr>', opts)
+keymap('n', '<c-left>',  ':vertical resize -4<cr>', opts)
+keymap('n', '<c-right>', ':vertical resize +4<cr>', opts)
+
+-- Move text up and down
+keymap("v", "<a-j>", "<esc>:m .+1<cr>", opts)
+keymap("v", "<a-k>", "<esc>:m .-2<cr>", opts)
+
+-- Identations
+keymap('v', '<', '<gv', opts)
+keymap('v', '>', '>gv', opts)
+
 
 -- Stop search highlight (eg. CancelHighlight)
 keymap('n', '<leader>h', ':nohlsearch<cr>', opts)
