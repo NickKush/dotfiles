c07507d0a76d21781202021f05262ccba1438927
NickKush <nickush42@gmail.com>
2022-06-23 22:41:02 +0300

    (nvim) cmp fix enter button

--- config/nvim-lua/lua/nickkush/cmp.lua
+++ config/nvim-lua/lua/nickkush/cmp.lua
@@ -12,7 +12,7 @@ cmp.setup({
       ["<c-f>"] = cmp.mapping.scroll_docs(4),
       ["<c-space>"] = cmp.mapping.complete(),
       ["<c-e>"] = cmp.mapping.abort(),
-      ["<cr>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      ["<cr>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
