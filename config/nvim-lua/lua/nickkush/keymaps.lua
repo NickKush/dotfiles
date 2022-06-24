3015b20fec2579c64f41bc0fd1ca1c837b406506
NickKush <nickush42@gmail.com>
2022-06-24 20:25:56 +0300

    (nvim) keymap for buffers

--- config/nvim-lua/lua/nickkush/keymaps.lua
+++ config/nvim-lua/lua/nickkush/keymaps.lua
@@ -52,6 +52,7 @@ keymap('t', '<a-l>', '<c-\\><c-n><c-w>l', opts)
 keymap('n', '<leader>n', "<cmd>lua require('telescope.builtin').find_files()<cr>", opts)
 keymap('n', '<F12>', "<cmd>lua require('telescope.builtin').help_tags()<cr>", opts)
 keymap("n", "<leader>f", "<cmd>lua require('telescope.builtin').live_grep()<cr>", opts)
+keymap("n", "<leader>b", "<cmd>lua require('telescope.builtin').buffers()<cr>", opts)
 
 -- File Tree
 keymap('n', '<F2>', "<cmd>lua require('nvim-tree').toggle()<cr>", opts)
