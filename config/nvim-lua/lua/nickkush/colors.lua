7c5b16cc166d515891eb4845b470975928cef190
NickKush <nickush42@gmail.com>
2022-10-25 21:54:04 +0300

    (nvim) add check for colorscheme

--- config/nvim-lua/lua/nickkush/colors.lua
+++ config/nvim-lua/lua/nickkush/colors.lua
@@ -1,4 +1,10 @@
-
 vim.g.gruvbox_invert_selection = "0"  -- Better selection color https://github.com/morhetz/gruvbox/issues/15
-vim.cmd[[colorscheme gruvbox]]
+
+local colorscheme = "gruvbox"
+
+local success, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not success then
+    vim.notify("Color scheme " .. colorscheme .. " not found")
+    return
+end
 
