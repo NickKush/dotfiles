46781189e260bdf7c19e6bfb680d93a4a8aa2bfd
NickKush <nickush42@gmail.com>
2022-10-27 01:40:42 +0300

    (bootstap) init bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Make sure we start this script with sudo/root 
+if [[ $EUID -ne 0 ]]; then
+   echo "this script must be run as root"
+   exit 1
+fi
+
+echo "Updating packages..."
+apt update -y
+
+echo "Install essential packages..."
+apt install -y curl \
+    xclip \
+    vim \
+    wget \
+    build-essential \
+    npm
+
+echo "Install stuff"
+for i in lazygit nvim; do
+    cd $i && bash install.sh
+    cd -
+done
+
+echo "Setup configuration"
+for i in lazygit nvim tmux; do
+    cd $i && bash setup.sh
+    cd -
+done
+
+
